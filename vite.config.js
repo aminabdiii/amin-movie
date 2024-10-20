5c9cecdedc23d3781c72628bca4877600269eecf
aminabdiii <aminabdiii2006@gmail.com>
2024-10-20 14:33:08 +0330

    add outDir build

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,21 @@ export default defineConfig({
   },
   build: {
     outDir: 'build',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        passes: 2,
+      },
+      mangle: {
+        properties: {
+          regex: /^_/,
+        },
+      },
+      output: {
+        comments: false,
+      },
+    },
   },
 });
