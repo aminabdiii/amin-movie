de6f89106950f33eec93154b9ebd0f3c6ae1b1a0
aminabdiii <aminabdiii2006@gmail.com>
2024-11-13 13:17:23 +0330

    fixed suspense and update packages

--- src/components/features/PageLoading.jsx
+++ src/components/features/PageLoading.jsx
@@ -2,8 +2,8 @@ import { CircularProgress } from '@mui/material';
 
 function PageLoading() {
   return (
-    <div className="flex justify-center items-center w-full min-h-screen bg-white absolute top-0 left-0">
-      <CircularProgress size={100} />
+    <div className="flex justify-center items-center w-full min-h-screen bg-white fixed top-0 left-0">
+      <CircularProgress size={100} className="!z-[9999]" />
     </div>
   );
 }
