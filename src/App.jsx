01bfe9ab5f703de795a02387a9a0c4d5da4c21eb
aminabdiii <aminabdiii2006@gmail.com>
2024-12-07 21:39:54 +0330

    design notfound page

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import { lazy, Suspense } from 'react';
 
 const HomePage = lazy(() => import('./pages/HomePage'));
 const AboutPage = lazy(() => import('./pages/AboutPage'));
-const ContactPage = lazy(() => import('./pages/ContactPage'));
-const SignInPage = lazy(() => import('./pages/SignInPage'));
 const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
 const WatchListPage = lazy(() => import('./pages/WatchListPage.jsx'));
 const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage'));
@@ -15,11 +13,9 @@ function App() {
     <BrowserRouter>
       <Suspense fallback={<PageLoading />}>
         <Routes>
-          <Route index element={<Navigate to={'/Home'} />} />
+          <Route index element={<Navigate to={'/Home'} replace />} />
           <Route path="/Home" element={<HomePage />} />
           <Route path="/About" element={<AboutPage />} />
-          <Route path="/Contact" element={<ContactPage />} />
-          <Route path="/SignIn" element={<SignInPage />} />
           <Route path="/watchlist" element={<WatchListPage />} />
           <Route path="/MovieDetails" element={<MovieDetailsPage />} />
           <Route path="*" element={<NotFoundPage />} />
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom';
+
 function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className="w-full h-dvh gap-10 flex flex-col justify-center items-center bg-gray-950 text-gray-100/60 font-bold font-sans">
+      <p className="text-6xl">404</p>
+      <Link
+        to="/"
+        className="p-5 border-gray-100/60 border rounded-md hover:bg-gray-100/60 hover:text-gray-950 hover:border-gray-950 transition-all duration-300 text-xl">
+        Back to Home
+      </Link>
+    </div>
+  );
 }
 
 export default NotFoundPage;
